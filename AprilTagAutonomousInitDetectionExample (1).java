9933a7fed0468d03b7cc32102a0f240ba37bb859
AdoptedKitty <65142948+AdoptedKitty@users.noreply.github.com>
2023-02-04 15:33:02 -0500

    add auto

--- AprilTagAutonomousInitDetectionExample (1).java
+++ AprilTagAutonomousInitDetectionExample (1).java
@@ -23,6 +23,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.openftc.apriltag.AprilTagDetection;
@@ -33,7 +34,7 @@ import org.openftc.easyopencv.OpenCvInternalCamera;
 
 import java.util.ArrayList;
 
-@TeleOp
+@Autonomous
 public class AprilTagAutonomousInitDetectionExample extends LinearOpMode
 {
 	OpenCvCamera camera;
